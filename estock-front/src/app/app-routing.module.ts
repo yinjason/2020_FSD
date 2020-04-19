01b28e6b05c51d919b941b678f7f4fee579ff18e
Jason <shlyyin@cn.ibm.com>
2020-04-19 23:04:10 +0800

    Add first login page and navigation

--- estock-front/src/app/app-routing.module.ts
+++ estock-front/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FsdLoginComponent } from '../fsd-components/fsd-login/fsd-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fsd-login', component: FsdLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ estock-front/src/fsd-components/fsd-login/fsd-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fsd-login',
+  templateUrl: './fsd-login.component.html',
+  styleUrls: ['./fsd-login.component.css']
+})
+export class FsdLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
